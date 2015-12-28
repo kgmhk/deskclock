1fe4ec9b8ea4a9b24b6761cd0cc1dd7ed759e77e
guru <gihyunkwak@naver.com>
2015-12-28 13:28:46 +0900

    테스트

--- app/src/main/java/com/gkwak/deskclock/MainActivity.java
+++ app/src/main/java/com/gkwak/deskclock/MainActivity.java
@@ -1,25 +1,41 @@
 package com.gkwak.deskclock;
 
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.gkwak.deskclock.coustomdialog.CustomDialog;
 import com.gkwak.deskclock.network.OpenWeatherAPITask;
 import com.gkwak.deskclock.weather.Weather;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 import java.util.concurrent.ExecutionException;
 
 //요것은 테스트임당 요것도
 public class MainActivity extends AppCompatActivity {
     CustomDialog dialog;
     TextView daily_text_view;
+    RelativeLayout r_layout;
+    LocationManager locationManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +44,17 @@ public class MainActivity extends AppCompatActivity {
 
         dialog = new CustomDialog(this);
         daily_text_view = (TextView)findViewById(R.id.daily_text_view);
+        r_layout = (RelativeLayout) findViewById(R.id.r_layout);
+        // Location
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        LocationListener locationListener = new MyLocationListener();
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, locationListener);
+        // BackGround
+        Resources res = getResources(); //resource handle
+        Drawable drawable = res.getDrawable(R.drawable.rain);
+
+
+
 
         daily_text_view.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,6 +73,7 @@ public class MainActivity extends AppCompatActivity {
 
             String temperature = String.valueOf(w.getTemprature());
 
+            r_layout.setBackground(drawable);
             //여기에 날씨를 비교하여 뒷 배경을 바꿔볼까?
 
 //            daily_text_view.setText(temperature);
@@ -57,6 +85,50 @@ public class MainActivity extends AppCompatActivity {
         } catch (ExecutionException e) {
             e.printStackTrace();
         }
+    }
+
+
+    /*---------- Listener class to get coordinates ------------- */
+    private class MyLocationListener implements LocationListener {
+
+        @Override
+        public void onLocationChanged(Location loc) {
+            Toast.makeText(
+                    getBaseContext(),
+                    "Location changed: Lat: " + loc.getLatitude() + " Lng: "
+                            + loc.getLongitude(), Toast.LENGTH_SHORT).show();
+            String longitude = "Longitude: " + loc.getLongitude();
+            System.out.println("long" + longitude);
+            String latitude = "Latitude: " + loc.getLatitude();
+            System.out.println("lat" + latitude);
+
+        /*------- To get city name from coordinates -------- */
+            String cityName = null;
+            Geocoder gcd = new Geocoder(getBaseContext(), Locale.getDefault());
+            List<Address> addresses;
+            try {
+                addresses = gcd.getFromLocation(loc.getLatitude(),
+                        loc.getLongitude(), 1);
+                if (addresses.size() > 0)
+                    System.out.println(addresses.get(0).getLocality());
+                cityName = addresses.get(0).getLocality();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            String s = longitude + "\n" + latitude + "\n\nMy Current City is: "
+                    + cityName;
+            Log.v("test", s);
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {}
+
+        @Override
+        public void onProviderEnabled(String provider) {}
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) {}
     }
 
     @Override
